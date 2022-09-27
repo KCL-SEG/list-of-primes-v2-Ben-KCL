b4e51df2c9ca6b72a546a2a25c45fd67aecc8378
Benjamin Groom <benjamin.groom@kcl.ac.uk>
2022-09-27 14:16:34 +0100

    Add solution code.

--- primes.py
+++ primes.py
@@ -2,5 +2,16 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+	if number_of_primes <= 0: 
+		raise ValueError(f'Non-positive values like {number_of_primes} are not permitted.')
+	list = []
+	num = 2
+	while len(list) < number_of_primes:
+		for divisor in range(2, num + 1):
+			if divisor == num:
+				list.append(num)
+			elif num % divisor == 0:
+				break
+		num += 1
+	return list
+
